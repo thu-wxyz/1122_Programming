f95a2c0455ae281960e07cfa664804b01b5e29c5
thu-wxyz <148457061+thu-wxyz@users.noreply.github.com>
2024-02-29 09:41:05 +0800

    Add files via upload
    week1_homework_part1

---
+++ week1_formula_solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+x1 = (-b+(b**2-4*a*c)**(0.5)) / (2*a)
+x2 = (-b-(b**2-4*a*c)**(0.5)) / (2*a)
+print("1st root is ", x1)
+print("2nd root is ", x2)
